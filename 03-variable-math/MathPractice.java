4699f9d58a68cd55fde83f069ae0c4e446a6cc9b
Colton <coltonjsooth@yahoo.com>
2025-04-16 15:11:26 -0700

    Adding Math Practice

---
+++ 03-variable-math/MathPractice.java
@@ -0,0 +1,22 @@
+public class MathPractice {
+    public static void main(String[] args) {
+        //declare the variables
+        int a = 10;
+        int b = 3;
+        double pi = 3.14159;
+        String name = "Colt";
+
+        //perform the math
+        int sum = a + b;
+        int product = a * b;
+        double average = (a + b) / 2.0;  
+
+        //print results
+        System.out.println("hello, " + name + "!");
+        System.out.println("Sum of a and b is: " + sum);
+        System.out.println("Product of a and b is: " + product);
+        System.out.println("Average of a and b is: " + average);
+
+        System.out.println("The value of Pi is: " + pi);
+    }
+}
